1505114b5162061e57cbe6253b1e2fb51354826e
Reto Schneider <code@reto-schneider.ch>
2014-06-08 05:06:41 +0200

    Add support for rule 18-0-3.

---
+++ src/rules/Rule_18_0_3.cpp
@@ -0,0 +1,49 @@
+//===-  Rule_18_0_3.cpp - Checker for MISRA C++ 2008 rule 18-0-3-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+#include <string>
+#include <set>
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+const static std::string ruleName = "18-0-3";
+
+class Rule_18_0_3 : public RuleCheckerASTContext,
+                    public RecursiveASTVisitor<Rule_18_0_3> {
+private:
+  static const std::set<std::string> illegalFunctions;
+
+public:
+  bool VisitDeclRefExpr(DeclRefExpr *expr) {
+    std::string funName = expr->getNameInfo().getAsString();
+    if (illegalFunctions.count(funName)) {
+      reportError("The library functions abort, exit, getenv and system from "
+                  "library <cstdlib> shall not be used.",
+                  expr->getLocStart());
+    }
+    return true;
+  }
+
+protected:
+  virtual void doWork() {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+const std::set<std::string> Rule_18_0_3::illegalFunctions = {
+    "abort", "exit", "getenv", "system"};
+
+static RuleCheckerASTContextRegistry::Add<Rule_18_0_3>
+X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
+}
---
+++ test/18-0-3.cpp
@@ -0,0 +1,17 @@
+// RUN: %clang -fsyntax-only -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 18-0-3 %s
+#include <cstdlib>
+void useAbort() {
+    abort(); // expected-error {{The library functions abort, exit, getenv and system from library <cstdlib> shall not be used.}}
+}
+
+void useExit() {
+    exit(EXIT_FAILURE); // expected-error {{The library functions abort, exit, getenv and system from library <cstdlib> shall not be used.}}
+}
+
+void useGetenv() {
+    getenv("HOME"); // expected-error {{The library functions abort, exit, getenv and system from library <cstdlib> shall not be used.}}
+}
+
+void useSystem() {
+    system("cat /proc/cpuinfo"); // expected-error {{The library functions abort, exit, getenv and system from library <cstdlib> shall not be used.}}
+}
