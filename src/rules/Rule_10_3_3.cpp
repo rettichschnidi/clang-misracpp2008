a4788f05666d7b0d35dcbf7e95ae0113eeec42a8
Reto Schneider <code@reto-schneider.ch>
2014-06-08 19:47:20 +0200

    Remove unnecessary header.

--- src/rules/Rule_10_3_3.cpp
+++ src/rules/Rule_10_3_3.cpp
@@ -6,7 +6,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "misracpp2008.h"
-#include "Rule_10_3_3.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/Basic/Diagnostic.h"
 
@@ -16,32 +16,38 @@ namespace misracpp2008 {
 
 const static std::string ruleName = "10-3-3";
 
-bool Rule_10_3_3::VisitCXXMethodDecl(clang::CXXMethodDecl *decl) {
-  if (decl->isPure()) {
-    auto B = context->overridden_methods_begin(decl);
-    auto E = context->overridden_methods_end(decl);
+class Rule_10_3_3 : public RuleCheckerASTContext,
+                    public clang::RecursiveASTVisitor<Rule_10_3_3> {
+public:
+  Rule_10_3_3() : RuleCheckerASTContext() {}
+  bool VisitCXXMethodDecl(clang::CXXMethodDecl *decl) {
+    if (decl->isPure()) {
+      auto B = context->overridden_methods_begin(decl);
+      auto E = context->overridden_methods_end(decl);
 
-    while (B != E) {
-      const CXXMethodDecl &m = **B;
-      if (m.isPure() == false) {
-        reportError("A virtual function shall only be overridden by a pure "
-                    "virtual function if it is itself declared as pure "
-                    "virtual.",
-                    decl->getLocation());
-        break;
+      while (B != E) {
+        const CXXMethodDecl &m = **B;
+        if (m.isPure() == false) {
+          reportError("A virtual function shall only be overridden by a pure "
+                      "virtual function if it is itself declared as pure "
+                      "virtual.",
+                      decl->getLocation());
+          break;
+        }
+        B++;
       }
-      B++;
     }
+    return true;
   }
-  return true;
-}
 
-Rule_10_3_3::~Rule_10_3_3() {}
+  virtual ~Rule_10_3_3() {}
 
-void Rule_10_3_3::doWork() {
-  RuleCheckerASTContext::doWork();
-  this->TraverseDecl(context->getTranslationUnitDecl());
-}
+protected:
+  virtual void doWork() {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
 
 static RuleCheckerASTContextRegistry::Add<Rule_10_3_3>
 X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
