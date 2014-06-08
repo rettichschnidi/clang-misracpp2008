82913724607a16622b02a84279a2b7fa5b16e9c0
Reto Schneider <code@reto-schneider.ch>
2014-06-09 00:58:56 +0200

    Add support for rule 16-3-1.

---
+++ src/rules/Rule_16_3_1.cpp
@@ -0,0 +1,48 @@
+//===-  Rule_16_3_1.cpp - Checker for MISRA C++ 2008 rule 16-3-1-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/Lex/Token.h"
+#include "clang/Basic/IdentifierTable.h"
+#include "clang/Lex/MacroInfo.h"
+#include <string>
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+const static std::string ruleName = "16-3-1";
+
+class Rule_16_3_1 : public RuleCheckerPreprocessor {
+private:
+public:
+  virtual void MacroDefined(const Token &MacroNameTok,
+                            const MacroDirective *MD) {
+    const MacroInfo *macroInfo = MD->getMacroInfo();
+    unsigned int counter = 0;
+
+    for (MacroInfo::tokens_iterator I = macroInfo->tokens_begin(),
+                                    E = macroInfo->tokens_end();
+         I != E; ++I) {
+      const std::string tokenType = I->getName();
+      // Count occurrences of "hash" and "hashhas"
+      if (tokenType.find_first_of("hashhash") == 0) {
+        counter++;
+        // Report only the first illegal hash
+        if (counter == 2) {
+          reportError("There shall be at most one occurrence of the # or ## "
+                      "operators in a single macro definition.",
+                      I->getLocation());
+        }
+      }
+    }
+  }
+};
+
+static RuleCheckerPreprocessorRegistry::Add<Rule_16_3_1>
+X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
+}
---
+++ test/16-3-1.cpp
@@ -0,0 +1,15 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 16-3-1 %s
+
+// Some allowed defines
+#define MAKE_STRING(s)  #s
+#define COMBINE(s1, s2) s1##s2
+#define MAKE_NOTHING(x)
+
+// Disallowed usages
+#define TRIPPLE_CONCAT(s) s##s##s // expected-error {{There shall be at most one occurrence of the # or ## operators in a single macro definition.}}
+#define DOUBLE_STRINGIFY(s1, s2) #s1#s2 // expected-error {{There shall be at most one occurrence of the # or ## operators in a single macro definition.}}
+#define STRINGIFY_CONCAT1(s1, s2) #s1##s2 // expected-error {{There shall be at most one occurrence of the # or ## operators in a single macro definition.}}
+#define STRINGIFY_CONCAT2(s1, s2) s1##s2#s1 // expected-error {{There shall be at most one occurrence of the # or ## operators in a single macro definition.}}
+#define STRINGIFY_MULTILINE(s1, s2) s1   \
+                                    ##s2 \
+                                    #s1  // expected-error {{There shall be at most one occurrence of the # or ## operators in a single macro definition.}}
