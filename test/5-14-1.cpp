ad8f9f28bc4509065b3aea1b8e9917d5e7a23144
Reto Schneider <code@reto-schneider.ch>
2014-07-22 22:56:23 +0200

    Improve tests for rule 5-14-1.

--- test/5-14-1.cpp
+++ test/5-14-1.cpp
@@ -16,21 +16,37 @@ int sideEffectsFunction() {
 }
 
 void volatileTest(int v) {
+    // Accessing a volatile variable on the right side
     if(v == 10 && volatileInteger == 10) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
     }
-
     if(v == 10 || volatileInteger == 10) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
     }
 
+    // Access the same volatile variable twice
     if(volatileInteger < 0 || volatileInteger > 10) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
     }
+    if(volatileInteger < 0 && volatileInteger > 10) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
+    }
+}
 
+void multipleViolations() {
     // This should generate two errors
     if((volatileInteger < 0 || volatileInteger > 10) && sideEffectsFunction()) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}} expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
     }
 }
 
+void modifyingVariableTest(int v) {
+    // Modifying a variable counts as side effect
+    if( v == 10 || v++ > 12) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
+    }
+    if( v == 10 && v++ > 12) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
+    }
+}
+
 void functionCallTest(int v) {
+    // Calling a function with side effects on the right hand side
     if(v == 10 && sideEffectsFunction() == 8) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
+    }
+    if(v == 10 || sideEffectsFunction() == 8) { // expected-error {{The right hand operand of a logical && or || operator shall not contain side effects.}}
     }
 }
