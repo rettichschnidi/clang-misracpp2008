94e7b7b88e2f7b5c681948eed79f7e4add267440
Reto Schneider <code@reto-schneider.ch>
2014-07-22 20:31:28 +0200

    Add support for rule 5-8-1.

---
+++ src/rules/Rule_5_8_1.cpp
@@ -0,0 +1,70 @@
+//===-  Rule_5_8_1.cpp - Checker for MISRA C++ 2008 rule 5-8-1-------------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright (C) 2014 Reto Schneider <code@reto-schneider.ch>
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+const static std::string ruleName = "5-8-1";
+
+class Rule_5_8_1 : public RuleCheckerASTContext,
+                   public RecursiveASTVisitor<Rule_5_8_1> {
+public:
+  Rule_5_8_1() : RuleCheckerASTContext() {}
+  bool VisitBinShl(BinaryOperator *S) { return isShiftStatementValid(S); }
+  bool VisitBinShr(BinaryOperator *S) { return isShiftStatementValid(S); }
+  bool VisitBinShrAssign(CompoundAssignOperator *S) {
+    return isShiftStatementValid(S);
+  }
+  bool VisitBinShlAssign(CompoundAssignOperator *S) {
+    return isShiftStatementValid(S);
+  }
+
+protected:
+  bool isShiftStatementValid(BinaryOperator *S) {
+    const Expr *additiveExpr = S->getRHS();
+    if (additiveExpr->isEvaluatable(*context)) {
+      const llvm::APSInt bitsToShift =
+          additiveExpr->EvaluateKnownConstInt(*context);
+      const unsigned integerSize = getBitWidthOfInteger(S->getLHS());
+
+      // Report an error if we can be sure (statically checked), that the number
+      // of bits to shift is either negative or greater-equal than the bit-size
+      // of the integer we are operating on.
+      if (bitsToShift.isNegative() || bitsToShift.uge(integerSize)) {
+        reportError(RULE_TEXT_5_8_1, S->getLocStart());
+      }
+    } else {
+      // Shifting can not statically be verified - review or runtime test
+      // needed!
+    }
+    return true;
+  }
+
+  unsigned getBitWidthOfInteger(const Expr *expr) {
+    const QualType &qualifierType = expr->IgnoreImpCasts()->getType();
+    assert(qualifierType->isIntegerType() &&
+           "Shift operators have to work on integers!");
+    return context->getIntWidth(qualifierType);
+  }
+
+  virtual void doWork() {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_5_8_1>
+X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
+}
---
+++ test/5-8-1.cpp
@@ -0,0 +1,81 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Wno-shift-count-negative -Wno-shift-count-overflow -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 5-8-1 %s
+
+#include <cstdint>
+
+#define SHIFT_2     2
+#define SHIFT_8     8
+#define SHIFT_NEG  -1
+
+void leftShiftFunction() {
+    uint8_t u8 = 1;
+    uint16_t u16;
+    const uint8_t shiftNum = 10;
+
+    u8 = (uint8_t)(u8 << 0); // Compliant - 0 is the lower bound
+    u8 = (uint8_t)(u8 << 7); // Compliant - 7 is the upper bound
+    u8 = (uint8_t)(u8 << 8); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 << (4 + 3)); // Compliant - 7
+    u8 = (uint8_t)(u8 << (4 + 4)); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 << -1); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 << shiftNum); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u16 = (uint16_t)((uint16_t)u8 << 15); // Compliant - 15 is the upper bound for a 16 bit value
+    u16 = (uint16_t)((uint16_t)u8 << 16); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+
+    // Using macros
+    u8 = (uint8_t)(u8 << SHIFT_2); // Compliant - the macro value is 2
+    u8 = (uint8_t)(u8 << SHIFT_8); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 << SHIFT_NEG); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+}
+
+void rightShiftFunction() {
+    uint8_t u8 = 1;
+    uint16_t u16;
+    const uint8_t shiftNum = 10;
+
+    u8 = (uint8_t)(u8 >> 0); // Compliant - 0 is the lower bound
+    u8 = (uint8_t)(u8 >> 7); // Compliant - 7 is the upper bound
+    u8 = (uint8_t)(u8 >> 8); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 >> (4 + 3)); // Compliant - 7
+    u8 = (uint8_t)(u8 >> (4 + 4)); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 >> -1); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 >> shiftNum); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u16 = (uint16_t)((uint16_t)u8 >> 15); // Compliant - 15 is the upper bound for a 16 bit value
+    u16 = (uint16_t)((uint16_t)u8 >> 16); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+
+    // Using macros
+    u8 = (uint8_t)(u8 >> SHIFT_2); // Compliant - the macro value is 2
+    u8 = (uint8_t)(u8 >> SHIFT_8); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 = (uint8_t)(u8 >> SHIFT_NEG); // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+}
+
+void leftShiftAssignFunction() {
+    uint8_t u8 = 1;
+    const uint8_t shiftNum = 10;
+
+    u8 <<= 0; // Compliant - 0 is the lower bound$
+    u8 <<= 7; // Compliant - 7 is the upper bound
+    u8 <<= 8; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 <<= shiftNum; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 <<= -1; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+
+    // Using macros
+    u8 <<= SHIFT_2; // Compliant - the macro value is 2
+    u8 <<= SHIFT_8; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 <<= SHIFT_NEG; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+}
+
+void rightShiftAssignFunction() {
+    uint8_t u8 = 1;
+    const uint8_t shiftNum = 10;
+
+    u8 >>= 0; // Compliant - 0 is the lower bound
+    u8 >>= 7; // Compliant - 7 is the upper bound
+    u8 >>= 8; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 >>= shiftNum; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 >>= -1; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+
+    // Using macros
+    u8 >>= SHIFT_2; // Compliant - the macro value is 2
+    u8 >>= SHIFT_8; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+    u8 >>= SHIFT_NEG; // expected-error {{The right hand operand of a shift operator shall lie between zero and one less than the width in bits of the underlying type of the left hand operand.}}
+}
