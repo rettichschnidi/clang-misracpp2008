941da333b067b5be0887c8f0da192636aae6c205
Reto Schneider <code@reto-schneider.ch>
2014-07-22 23:42:28 +0200

    Add support for rule 5-18-1.

---
+++ src/rules/Rule_5_18_1.cpp
@@ -0,0 +1,40 @@
+//===-  Rule_5_18_1.cpp - Checker for MISRA C++ 2008 rule 5-18-1-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright (C) 2014 Reto Schneider <code@reto-schneider.ch>
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+const static std::string ruleName = "5-18-1";
+
+class Rule_5_18_1 : public RuleCheckerASTContext,
+                    public RecursiveASTVisitor<Rule_5_18_1> {
+public:
+  Rule_5_18_1() : RuleCheckerASTContext() {}
+
+  bool VisitBinComma(const BinaryOperator *S) {
+    reportError(RULE_TEXT_5_18_1, S->getLocStart());
+    return true;
+  }
+
+protected:
+  virtual void doWork() {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_5_18_1>
+X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
+}
--- test/5-18-1.cpp
+++ test/5-18-1.cpp
@@ -1,13 +1,27 @@
-// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 5-18-1 %s
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Wno-unused-value -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 5-18-1 %s
 
 
-int a, b; // expect-error {{The comma operator shall not be used.}}
+int a, t, c;
+bool retValue = true;
 
 // Auxiliary function
-int function(int a, int b) {
-    return a + b;
+int f(int x, int y, int z) {
+    return x + y + z;
 }
 
 int violatingFunctionCall() {
-    function( (1, 10), 50);  // expect-error {{The comma operator shall not be used.}}
+    return f(a, (t=3, t+2), c); // expected-error {{The comma operator shall not be used.}}
+}
+
+void violatingForLoop() {
+    for(a = 0, c = a + 10; a < c; a++) { // expected-error {{The comma operator shall not be used.}}
+    }
+}
+
+int violatingReturn() {
+    return (retValue = false, 5); // expected-error {{The comma operator shall not be used.}}
+}
+
+void violatingAssignement(int i) {
+    if(i == 10) a = 10, c = 10; // expected-error {{The comma operator shall not be used.}}
 }
