fd0e3a93683aa8c96c61624e77f3d85a1ba8ada7
Reto Schneider <code@reto-schneider.ch>
2014-07-24 00:01:11 +0200

    Add some MISRA C++:2008 violating code.

--- examples/OffendingProject/main.cpp
+++ examples/OffendingProject/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
 
-using namespace std;
+#define __DATE__ "TODAY"
 
-int main() {
-  cout << "Hello World!" << endl;
+int getSquare(int i) { return i * i; }
+
+int main(int argc, char **argv) {
+  int i;
+  if (argc == 2) {
+    i = atoi(argv[1]);
+    std::cout << "Square of " << i << " is " << getSquare(i) << '\n';
+  } else {
+    std::cout << "Usage: " << argv[0] << " <number>\n";
+  }
   return 0;
 }
