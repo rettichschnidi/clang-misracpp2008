1b1324c7c3a61f5930e32bf716e627aca082ba5c
Reto Schneider <code@reto-schneider.ch>
2014-08-01 08:42:28 +0200

    Add support for rule 6-2-1.

---
+++ src/rules/Rule_6_2_1.cpp
@@ -0,0 +1,66 @@
+//===-  Rule_6_2_1.cpp - Checker for MISRA C++ 2008 rule 6-2-1-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright (C) 2014 Reto Schneider <code@reto-schneider.ch>
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+#include <set>
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+const static std::string ruleName = "6-2-1";
+
+class Rule_6_2_1 : public RuleCheckerASTContext,
+                   public RecursiveASTVisitor<Rule_6_2_1> {
+public:
+  Rule_6_2_1() : RuleCheckerASTContext() {}
+
+  bool VisitStmt(Stmt *S) {
+    if (isa<Expr>(S) == false)
+      return true;
+
+    for (Stmt::child_iterator I = S->child_begin(), E = S->child_end(); I != E;
+         ++I) {
+      if (BinaryOperator *bo = dyn_cast<BinaryOperator>(I->IgnoreImplicit())) {
+        if (bo->getOpcode() == BO_Assign) {
+          reportViolatingStatement(bo);
+        }
+      }
+      if (CXXOperatorCallExpr *oc =
+              dyn_cast<CXXOperatorCallExpr>(I->IgnoreImplicit())) {
+        if (oc->getOperator() == OO_Equal) {
+          reportViolatingStatement(oc);
+        }
+      }
+    }
+    return true;
+  }
+
+private:
+  std::set<const Stmt *> reportedStatements;
+  void reportViolatingStatement(const Stmt *stmt) {
+    if (reportedStatements.find(stmt) == reportedStatements.end()) {
+      reportedStatements.insert(stmt);
+      reportError(RULE_TEXT_6_2_1, stmt->getLocStart());
+    }
+  }
+
+protected:
+  virtual void doWork() override {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_6_2_1>
+    X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
+}
---
+++ test/6-2-1.cpp
@@ -0,0 +1,44 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Wno-parentheses -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 6-2-1 %s
+
+int f1();
+
+void f(int a, int b) {
+    int z = 1;
+    int zz = a = 2; // expected-error {{Assignment operators shall not be used in sub-expressions.}}
+    int t = a != b;
+
+    b = b; // Compliant
+
+    a = b = zz; // expected-error {{Assignment operators shall not be used in sub-expressions.}}
+
+    if( a ) { // Compliant
+    }
+    if( a = b ) { // expected-error {{Assignment operators shall not be used in sub-expressions.}}
+    }
+    if( ( a = b ) ) { // expected-error {{Assignment operators shall not be used in sub-expressions.}}
+    }
+
+    if( int v = a ) { // Compliant
+    }
+    if( int v = f1()) { // Compliant
+    }
+
+    while( int v = f1()) { // Compliant
+    }
+    while( z = f1()) { // expected-error {{Assignment operators shall not be used in sub-expressions.}}
+    }
+}
+
+struct S {
+    int i;
+    operator bool() { return i == 1; }
+};
+
+void complicatedCases(S s) {
+    if( S s1 = s ) { // Compliant
+    }
+
+    S s2;
+    if( s2 = s) { // expected-error {{Assignment operators shall not be used in sub-expressions.}}
+    }
+}
