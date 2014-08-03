b11fc86fe589e66fdf6fc1203f3f8f52b5ec0635
Reto Schneider <code@reto-schneider.ch>
2014-08-03 03:16:23 +0200

    Add support for rule 6-4-1.

---
+++ src/rules/Rule_6_4_1.cpp
@@ -0,0 +1,57 @@
+//===-  Rule_6_4_1.cpp - Checker for MISRA C++ 2008 rule 6-4-1-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright (C) 2014 Reto Schneider <code@reto-schneider.ch>
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+const static std::string ruleName = "6-4-1";
+
+class Rule_6_4_1 : public RuleCheckerASTContext,
+                   public RecursiveASTVisitor<Rule_6_4_1> {
+public:
+  Rule_6_4_1() : RuleCheckerASTContext() {}
+
+  bool VisitStmt(Stmt *S) {
+    if (const IfStmt *is = dyn_cast<IfStmt>(S)) {
+      const Stmt *thenStmt = is->getThen();
+      const Stmt *elseStmt = is->getElse();
+      // Check if then has a body
+      if (thenStmt && isa<CompoundStmt>(thenStmt) == false) {
+        ReportError(thenStmt->getLocStart());
+      }
+      // Check if else has a body or another if statement
+      if (elseStmt &&
+          ((isa<CompoundStmt>(elseStmt) || isa<IfStmt>(elseStmt)) == false)) {
+        ReportError(elseStmt->getLocStart());
+      }
+    }
+    return true;
+  }
+
+private:
+  void ReportError(const SourceLocation loc) {
+    RuleCheckerASTContext::reportError(RULE_TEXT_6_4_1, loc);
+  }
+
+protected:
+  virtual void doWork() override {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_6_4_1>
+    X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
+}
---
+++ test/6-4-1.cpp
@@ -0,0 +1,31 @@
+// RUN: %clang -fsyntax-only -std=c++11 -Wno-empty-body -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 6-4-1 %s
+
+void compliant(int x, int y, int z) {
+    if(x) {
+    }
+
+    if(x) {
+    } else if(y) {
+    }
+
+    if(x) {
+    } else if(y) {
+    } else {
+    }
+}
+
+void violations(int x, int y, int z) {
+    if(x); // expected-error {{An if ( condition ) construct shall be followed by a compound statement. The else keyword shall be followed by either a compound statement, or another if statement. (MISRA C++ 2008 rule 6-4-1)}}
+
+    if(x)
+        y = 1; // expected-error {{An if ( condition ) construct shall be followed by a compound statement. The else keyword shall be followed by either a compound statement, or another if statement. (MISRA C++ 2008 rule 6-4-1)}}
+
+    if(x) {
+    } else
+        y = 1; // expected-error {{An if ( condition ) construct shall be followed by a compound statement. The else keyword shall be followed by either a compound statement, or another if statement. (MISRA C++ 2008 rule 6-4-1)}}
+
+    if(x) {
+    } else if(y) {
+    } else
+        z = 1;  // expected-error {{An if ( condition ) construct shall be followed by a compound statement. The else keyword shall be followed by either a compound statement, or another if statement. (MISRA C++ 2008 rule 6-4-1)}}
+}
