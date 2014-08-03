fc2d1795e8b27ed7f1ef57979529085600b69263
Reto Schneider <code@reto-schneider.ch>
2014-08-03 02:39:19 +0200

    Add support for rule 6-3-1.

---
+++ src/rules/Rule_6_3_1.cpp
@@ -0,0 +1,53 @@
+//===-  Rule_6_3_1.cpp - Checker for MISRA C++ 2008 rule 6-3-1-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright (C) 2014 Reto Schneider <code@reto-schneider.ch>
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+const static std::string ruleName = "6-3-1";
+
+class Rule_6_3_1 : public RuleCheckerASTContext,
+                   public RecursiveASTVisitor<Rule_6_3_1> {
+public:
+  Rule_6_3_1() : RuleCheckerASTContext() {}
+
+  bool VisitStmt(Stmt *S) {
+    if (ForStmt *fs = dyn_cast<ForStmt>(S)) {
+      ReportIfNullStmt(fs->getBody());
+    } else if (DoStmt *ds = dyn_cast<DoStmt>(S)) {
+      ReportIfNullStmt(ds->getBody());
+    } else if (WhileStmt *ws = dyn_cast<WhileStmt>(S)) {
+      ReportIfNullStmt(ws->getBody());
+    }
+    return true;
+  }
+
+public:
+  void ReportIfNullStmt(const Stmt *S) {
+    if (isa<CompoundStmt>(S) != true) {
+      reportError(RULE_TEXT_6_3_1, S->getLocStart());
+    }
+  }
+
+protected:
+  virtual void doWork() override {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_6_3_1>
+    X(ruleName.c_str(), "MISRA C++ 2008 rule checker");
+}
---
+++ test/6-3-1.cpp
@@ -0,0 +1,22 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 6-3-1 %s
+
+volatile int cond;
+
+void compliant() {
+    for(int i = 0; i < 100; ++i) {
+    }
+
+    do {} while(cond);
+
+    while(cond) {}
+}
+
+void violations() {
+    for(int i = 0; i < 100; ++i); // expected-error {{The statement forming the body of a switch, while, do ... while or for statement shall be a compound statement. (MISRA C++ 2008 rule 6-3-1)}}
+
+    do; while(cond); // expected-error {{The statement forming the body of a switch, while, do ... while or for statement shall be a compound statement. (MISRA C++ 2008 rule 6-3-1)}}
+
+    while(cond); // expected-error {{The statement forming the body of a switch, while, do ... while or for statement shall be a compound statement. (MISRA C++ 2008 rule 6-3-1)}}
+
+    while(cond) { cond++; } // this again is valid
+}
