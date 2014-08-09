f9ca5181507c650abbebafc43954f9d8a623e2ee
Reto Schneider <code@reto-schneider.ch>
2014-08-09 14:30:52 +0200

    Add support for rule 2-10-2.

---
+++ src/rules/Rule_2_10_2.cpp
@@ -0,0 +1,83 @@
+//===-  Rule_2_10_2.cpp - Checker for MISRA C++ 2008 rule 2-10-2-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright (C) 2014 Reto Schneider <code@reto-schneider.ch>
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+#include <map>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <functional>
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+class Rule_2_10_2 : public RuleCheckerASTContext,
+                    public RecursiveASTVisitor<Rule_2_10_2> {
+public:
+  Rule_2_10_2() : RuleCheckerASTContext() {}
+
+  bool VisitDecl(Decl *decl) {
+    // Bail out early if this location should not be checked.
+    if (doIgnore(decl->getLocation())) {
+      return true;
+    }
+    // Bail out if this is not a named declaration.
+    if (!isa<NamedDecl>(decl)) {
+      return true;
+    }
+
+    const NamedDecl *namedDecl = cast<NamedDecl>(decl);
+    const DeclarationName &declN = namedDecl->getDeclName();
+    // Bail out if it is an operator
+    if (declN.getCXXOverloadedOperator() != OO_None) {
+      return true;
+    }
+
+    // Walk up till we reach the outermost scope, check each one for a
+    // declaration with the same name.
+    for (const DeclContext *outerScope =
+             namedDecl->getDeclContext()->getParent();
+         outerScope != nullptr; outerScope = outerScope->getParent()) {
+      if (isa<LinkageSpecDecl>(outerScope)) {
+        continue;
+      }
+
+      DeclContext::lookup_const_result result = outerScope->lookup(declN);
+      if (result.empty()) {
+        continue;
+      }
+      // Bail out if we find ourself
+      if (std::find(result.begin(), result.end(), namedDecl) != result.end()) {
+        continue;
+      }
+
+      // All hope is lost - we have a shadowing declaration.
+      // Report location of the offending and the shadowed declaration.
+      reportError(decl->getLocation());
+      reportError(result[0]->getLocation(),
+                  "Previous occurence of identifier '%0'")
+          << declN.getAsString();
+    }
+    return true;
+  }
+
+protected:
+  virtual void doWork() override {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_2_10_2> X("2-10-2",
+                                                         RULE_TEXT_2_10_2);
+}
---
+++ test/2-10-2.cpp
@@ -0,0 +1,31 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 2-10-2 %s
+
+int a; // expected-error {{Previous occurence of identifier 'a'}}
+
+void fnA(int a) { // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
+}
+
+int b; // expected-error {{Previous occurence of identifier 'b'}}
+extern "C" {
+  void fnB() {
+    char b; // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
+  }
+}
+
+int c; // expected-error {{Previous occurence of identifier 'c'}}
+void fnB2() {
+  char c; // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
+}
+
+int d; // expected-error {{Previous occurence of identifier 'd'}}
+int e; // expected-error {{Previous occurence of identifier 'e'}}
+class MyClass {
+  int d; // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
+  void myMethod(int e); // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
+  void someMethod(); // expected-error {{Previous occurence of identifier 'someMethod'}}
+  void operator++() {}
+  class MyInnerClass {
+    void someMethod(); // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
+    void operator++() {}
+  };
+};
