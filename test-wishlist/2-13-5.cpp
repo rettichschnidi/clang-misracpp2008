e71389f610f35e5ad3b6cba38d19204d891ab10f
Reto Schneider <code@reto-schneider.ch>
2014-09-01 23:45:03 +0200

    Add test for rule 2-13-5 to wishlist.

---
+++ test-wishlist/2-13-5.cpp
@@ -0,0 +1,5 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 2-13-5 %s
+
+char    narrowChars[]   = "Hello" " narrow chars."; // Compliant
+wchar_t wideChars[]     = L"Hello" L" wide chars."; // Compliant
+wchar_t mixedChars[]    = "Hello" L" narrow and mixed chars.";  // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
