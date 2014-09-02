6911c16ee6d71ce57154f2805d9fac923d6d8ce1
Reto Schneider <code@reto-schneider.ch>
2014-09-02 23:48:56 +0200

    Add tests for rule 3-1-1.

---
+++ test/3-1-1.cpp
@@ -0,0 +1,4 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 3-1-1 %s
+
+#include "3-1-1.h"
+
---
+++ test/3-1-1.h
@@ -0,0 +1,7 @@
+void f(); // compliant
+int inline g(int i) { return i * 2; } // compliant
+template<typename T> T qubicT(T t) { return t * t; } // compliant
+
+int a; // expected-error {{It shall be possible to include any header file in multiple translation units without violating the One Definition Rule. (MISRA C++ 2008 rule 3-1-1)}}
+int h(int i) { return i * 3; }  // expected-error {{It shall be possible to include any header file in multiple translation units without violating the One Definition Rule. (MISRA C++ 2008 rule 3-1-1)}}
+int qubic(int t) { return t * t; } // expected-error {{It shall be possible to include any header file in multiple translation units without violating the One Definition Rule. (MISRA C++ 2008 rule 3-1-1)}}
