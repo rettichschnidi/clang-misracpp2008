4ec044418b3bb847742991477e4cd0554f0cd8b8
Reto Schneider <code@reto-schneider.ch>
2014-09-22 00:58:35 +0200

    Improve tests for rule 2-13-4, expose bug.

--- test/2-13-4.cpp
+++ test/2-13-4.cpp
@@ -1,15 +1,17 @@
 // RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 2-13-4 %s
 
+
 unsigned int u1 = 1U; // Compliant
 unsigned int u2 = 1u; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
 
-unsigned int l1 = 1L; // Compliant
-unsigned int l2 = 1l; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
+long l1 = 1L; // Compliant
+long l2 = 1l; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
 
-unsigned int ul1 = 0x1bUL; // Compliant
-unsigned int ul2 = 0x1buL; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
-unsigned int ul3 = 1Ul; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
-unsigned int ul4 = 1ul; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
+unsigned long ul0 = 0x1fUL; // Compliant
+unsigned long ul1 = 0x1bUL; // Compliant
+unsigned long ul2 = 0x1buL; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
+unsigned long ul3 = 1Ul; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
+unsigned long ul4 = 1ul; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
 
 float f1 = 1.0F; // Compliant
 float f2 = 1.0f; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
@@ -18,3 +20,7 @@ void func(float &f) {
   f = 10.0F; // Compliant
   f = 10.0f; // expected-error {{Literal suffixes shall be upper case. (MISRA C++ 2008 rule 2-13-4)}}
 }
+
+// Regression test - macros got misinterpreted
+#define STX_ETX_BUFFER_SIZE   180 ///< ulf
+unsigned char buffer[STX_ETX_BUFFER_SIZE]; // Compliant
