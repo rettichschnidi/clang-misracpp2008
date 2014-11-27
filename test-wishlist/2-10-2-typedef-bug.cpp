6847d50e3b7038aa8bded9e6da31671e4016777e
Reto Schneider <code@reto-schneider.ch>
2014-11-27 03:57:50 +0100

    Checker for rule 2-10-2 fails on typedef-ed constructs. Add some test-code.

---
+++ test-wishlist/2-10-2-typedef-bug.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 2-10-2 %s
+
+typedef union {
+  struct {
+    int element;
+  };
+} TypedefStruct;
+
+TypedefStruct x;
+TypedefStruct y;
