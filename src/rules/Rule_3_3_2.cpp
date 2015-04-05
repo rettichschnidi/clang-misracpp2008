ec01e65fa7b6730ca1e72cfd93749bb46c68dd11
Reto Schneider <code@reto-schneider.ch>
2015-04-05 03:27:52 +0200

    Add support for rule 3-3-2.

---
+++ src/rules/Rule_3_3_2.cpp
@@ -0,0 +1,48 @@
+//===-  Rule_3_3_2.cpp - Checker for MISRA C++ 2008 rule 3-3-2-----------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+class Rule_3_3_2 : public RuleCheckerASTContext,
+                   public clang::RecursiveASTVisitor<Rule_3_3_2> {
+public:
+  Rule_3_3_2() : RuleCheckerASTContext() {}
+
+  bool VisitFunctionDecl(const FunctionDecl *D) {
+    if (doIgnore(D->getLocStart())) {
+      return true;
+    }
+
+    if ((D->getStorageClass() == SC_Static) &&
+        (D->isCXXClassMember() == false)) {
+      for (const auto RI : D->redecls()) {
+        if (RI->getStorageClass() != SC_Static) {
+          reportError(RI->getLocation());
+          break;
+        }
+      }
+    }
+
+    return true;
+  }
+
+protected:
+  virtual void doWork() override {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_3_3_2> X("3-3-2", "");
+}
---
+++ test/3-3-2.cpp
@@ -0,0 +1,17 @@
+// RUN: %clang -fsyntax-only -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 3-3-2 %s
+
+static void compliantStaticFunc();
+static void compliantStaticFunc() {}
+
+void compliantNonStaticFunc();
+void compliantNonStaticFunc() {}
+
+static void nonCompliantStaticFun();
+void nonCompliantStaticFun() { // expected-error {{If a function has internal linkage then all re-declarations shall include the static storage class specifier. (MISRA C++ 2008 rule 3-3-2)}}
+}
+
+class A {
+  static void staticMethod();
+};
+
+void A::staticMethod() {}
