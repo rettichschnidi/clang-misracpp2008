720d23fffa41c396c994da4af97a22df1562e697
Reto Schneider <code@reto-schneider.ch>
2015-05-17 23:26:39 +0200

    Add support for rule 3-9-2.

---
+++ src/rules/Rule_3_9_2.cpp
@@ -0,0 +1,75 @@
+//===-  Rule_3_9_2.cpp - Checker for MISRA C++ 2008 rule 3-9-2-------------===//
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "misracpp2008.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/Basic/Diagnostic.h"
+#include "llvm/ADT/StringExtras.h"
+
+using namespace clang;
+
+namespace misracpp2008 {
+
+class Rule_3_9_2 : public RuleCheckerASTContext,
+                   public RecursiveASTVisitor<Rule_3_9_2> {
+public:
+  Rule_3_9_2() : RuleCheckerASTContext() {}
+
+  bool VisitVarDecl(const VarDecl *D) {
+    // Bail out early if this location should not be checked.
+    if (doIgnore(D->getLocation())) {
+      return true;
+    }
+
+    const QualType qualType = D->getType();
+    // Bail out if this type is either an enum or does not look like a real
+    // value.
+    if (qualType->isEnumeralType() || qualType->isBooleanType() ||
+        qualType->isArithmeticType() == false) {
+      return true;
+    }
+
+    const Type *t = qualType.getTypePtrOrNull();
+    assert(t && "Type of arithmetic types has to be available.");
+    const std::string typeName = qualType.getAsString();
+    // If it is of the same type as "size_t" and does have "size_t" somewhere in
+    // its name we can go with it.
+    // Please note: This also allows a typedef for "unsigned long" to be named
+    // e.g. "size_type" without any size indicator - which may or may not be a
+    // good thing.
+    if (context->hasSameUnqualifiedType(qualType, context->getSizeType()) &&
+        typeName.find("size_t") != std::string::npos) {
+      return true;
+    }
+
+    // char_t and wchar_t are not subject to this rule.
+    const std::string needle = "char_t";
+    if (std::equal(needle.rbegin(), needle.rend(), typeName.rbegin())) {
+      return true;
+    }
+
+    const uint64_t typeSize = context->getTypeSize(t);
+    const std::string sizeStr = llvm::utostr(typeSize);
+    // For all remaining types, the number of occupied bits must be embedded in
+    // the typename.
+    if (typeName.rfind(sizeStr) == std::string::npos) {
+      reportError(D->getLocation());
+    }
+
+    return true;
+  }
+
+protected:
+  virtual void doWork() override {
+    RuleCheckerASTContext::doWork();
+    this->TraverseDecl(context->getTranslationUnitDecl());
+  }
+};
+
+static RuleCheckerASTContextRegistry::Add<Rule_3_9_2> X("3-9-2", "");
+}
---
+++ test/3-9-2.cpp
@@ -0,0 +1,45 @@
+// RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 3-9-2 %s
+
+#include <cstdint>
+#include <cstddef>
+
+signed char nonCompliantSignedChar; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+unsigned char nonCompliantUnsignedChar; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+signed int nonCompliantSignedInt; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+unsigned int nonCompliantUnsignedInt; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+signed short nonCompliantSignedShort; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+unsigned short nonCompliantUnsignedShort; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+signed long nonCompliantSignedLong; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+unsigned long nonCompliantUnsignedLong; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+float nonCompliantFloat; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+double nonCompliantDouble; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+long double nonCompliantLongDouble; // expected-error {{typedefs that indicate size and signedness should be used in place of the basic numerical types. (MISRA C++ 2008 rule 3-9-2)}}
+
+typedef char char_t;
+typedef float float32;
+typedef double float64;
+typedef long double float128;
+namespace mynamespace {
+  typedef char char_t;
+}
+
+// A bunch of compliant definitions
+char_t compliantChar_t;
+mynamespace::char_t compliantNsChar_t;
+uint8_t compliantUint8_t;
+uint16_t compliantUint16_t;
+uint32_t compliantUint32_t;
+uint64_t compliantUint64_t;
+float32 compliantFloat32;
+float64 compliantFloat64;
+float128 compliantFloat128;
+bool compliantBool;
+size_t compliantSize_t;
+const size_t compliantConstSize_t = 1;
+
+enum Enum {
+  ENUM_ENTRY_A,
+  ENUM_ENTRY_B
+};
+
+Enum e = ENUM_ENTRY_A;
