7db27512ce2ef38c0fffd9b1c38ffd1b31113be5
Reto Schneider <code@reto-schneider.ch>
2015-05-30 04:37:41 +0200

    Expose hints not reported as notes.

--- test/2-10-1.cpp
+++ test/2-10-1.cpp
@@ -1,36 +1,36 @@
 // RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 2-10-1 %s
 
 // Only difference is caseness
-int abc; // expected-error {{Typographically ambiguous identifier 'abc'}}
+int abc; // expected-note {{Typographically ambiguous identifier 'abc'}}
 int aBc; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
 // Only difference is the underscore
-int b_b_; // expected-error {{Typographically ambiguous identifier 'b_b_'}} // expected-error {{Typographically ambiguous identifier 'b_b_'}}
-int bb; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}} // expected-error {{Typographically ambiguous identifier 'bb'}}
+int b_b_; // expected-note {{Typographically ambiguous identifier 'b_b_'}} // expected-note {{Typographically ambiguous identifier 'b_b_'}}
+int bb; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}} // expected-note {{Typographically ambiguous identifier 'bb'}}
 int b___b; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int c0; // expected-error {{Typographically ambiguous identifier 'c0'}}
+int c0; // expected-note {{Typographically ambiguous identifier 'c0'}}
 int cO; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int dI; // expected-error {{Typographically ambiguous identifier 'dI'}}
+int dI; // expected-note {{Typographically ambiguous identifier 'dI'}}
 int d1; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int el; // expected-error {{Typographically ambiguous identifier 'el'}}
+int el; // expected-note {{Typographically ambiguous identifier 'el'}}
 int eI; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int fl; // expected-error {{Typographically ambiguous identifier 'fl'}}
+int fl; // expected-note {{Typographically ambiguous identifier 'fl'}}
 int f1; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int gS; // expected-error {{Typographically ambiguous identifier 'gS'}}
+int gS; // expected-note {{Typographically ambiguous identifier 'gS'}}
 int g5; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int hZ; // expected-error {{Typographically ambiguous identifier 'hZ'}}
+int hZ; // expected-note {{Typographically ambiguous identifier 'hZ'}}
 int h2; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int iB; // expected-error {{Typographically ambiguous identifier 'iB'}}
+int iB; // expected-note {{Typographically ambiguous identifier 'iB'}}
 int i8; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
-int jrn; // expected-error {{Typographically ambiguous identifier 'jrn'}}
+int jrn; // expected-note {{Typographically ambiguous identifier 'jrn'}}
 int jm; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 
 // Regression test: Typedefs of an anoymous type should not get reported.
@@ -42,7 +42,7 @@ typedef enum {
   ENUM2_ENTRY_ONE
 } myEnum2;
 
-void func (int aVariable, int _anotherVariable) { // expected-error {{Typographically ambiguous identifier 'aVariable'}}  { // expected-error {{Typographically ambiguous identifier '_anotherVariable'}}
+void func (int aVariable, int _anotherVariable) { // expected-note {{Typographically ambiguous identifier 'aVariable'}}  { // expected-note {{Typographically ambiguous identifier '_anotherVariable'}}
   int a_Variable; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
   int anotherVariable; // expected-error {{Different identifiers shall be typographically unambiguous. (MISRA C++ 2008 rule 2-10-1)}}
 }
--- test/2-10-2.cpp
+++ test/2-10-2.cpp
@@ -1,28 +1,28 @@
 // RUN: %clang -fsyntax-only -std=c++11 -ferror-limit=0 -Xclang -verify -Xclang -load -Xclang %llvmshlibdir/misracpp2008.so -Xclang -plugin -Xclang misra.cpp.2008 -Xclang -plugin-arg-misra.cpp.2008 -Xclang 2-10-2 %s
 
-int a; // expected-error {{Previous occurence of identifier 'a'}}
+int a; // expected-note {{Previous occurence of identifier 'a'}}
 
 void fnA(int a) { // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
 }
 
-int b; // expected-error {{Previous occurence of identifier 'b'}}
+int b; // expected-note {{Previous occurence of identifier 'b'}}
 extern "C" {
   void fnB() {
     char b; // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
   }
 }
 
-int c; // expected-error {{Previous occurence of identifier 'c'}}
+int c; // expected-note {{Previous occurence of identifier 'c'}}
 void fnB2() {
   char c; // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
 }
 
-int d; // expected-error {{Previous occurence of identifier 'd'}}
-int e; // expected-error {{Previous occurence of identifier 'e'}}
+int d; // expected-note {{Previous occurence of identifier 'd'}}
+int e; // expected-note {{Previous occurence of identifier 'e'}}
 class MyClass {
   int d; // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
   void myMethod(int e); // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
-  void someMethod(); // expected-error {{Previous occurence of identifier 'someMethod'}}
+  void someMethod(); // expected-note {{Previous occurence of identifier 'someMethod'}}
   void operator++() {}
   class MyInnerClass {
     void someMethod(); // expected-error {{Identifiers declared in an inner scope shall not hide an identifier declared in an outer scope. (MISRA C++ 2008 rule 2-10-2)}}
